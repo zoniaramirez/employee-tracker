563040436c208a53463dbc573c149bf1d4594daf
Zonia Ramirez <jslnramirez@gmail.com>
2024-07-26 22:08:51 -0400

    updated prompts.js

---
+++ prompts.js
@@ -0,0 +1,23 @@
+const inquirer = require('inquirer');
+
+const question = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ]);
+};
+
+module.exports = { question };
